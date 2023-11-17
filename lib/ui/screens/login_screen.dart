a2cc4c92d9fc6dfb6e03d2e254b8274bc5b00718
Kabir Giuralarocca <kabir.giuralarocca@reti.it>
2023-11-17 12:16:57 +0100

    fix target platform

--- lib/ui/screens/login_screen.dart
+++ lib/ui/screens/login_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_movies_app/data/repositories/apk_repository.dart';
@@ -142,7 +140,8 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               height_8,
               Visibility(
-                visible: kIsWeb && Platform.isAndroid,
+                visible:
+                    kIsWeb && defaultTargetPlatform == TargetPlatform.android,
                 child: DownloadButton(
                   label: "Download APK",
                   onPressed: () => _downloadApk(),
